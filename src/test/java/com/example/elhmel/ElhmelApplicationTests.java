4b04da8912d6c8e9eeae77cc83c1af6205402186
yarporokh <yaroslavporohnenko@gmail.com>
2023-01-17 21:01:19 +0200

    Init project

---
+++ src/test/java/com/example/elhmel/ElhmelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elhmel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElhmelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
