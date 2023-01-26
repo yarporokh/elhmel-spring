91d61636cd75add0f5fa3b69a0645dd4e44b790b
yarporokh <yaroslavporohnenko@gmail.com>
2023-01-26 14:03:29 +0200

    Added security config

---
+++ src/main/java/com/example/elhmel/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.elhmel.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/*").permitAll()
+               // .anyRequest().permitAll()
+                .and()
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .permitAll()).logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .permitAll()
+                );
+
+
+        return http.build();
+    }
+}
